b4c2eb8b79c91ecbd6226ef28a7e720af6ea7ab4
pepesan <pepesan@gmail.com>
2016-05-18 22:42:09 +0200

    colocando una clase de manera condicional

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
     template:`
   <h2>My Heroes</h2>
 <ul class="heroes">
-  <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+  <li *ngFor="let hero of heroes" (click)="onSelect(hero)" 
+  [class.selected]="hero === selectedHero">
     <span class="badge">{{hero.id}}</span> {{hero.name}}
   </li>
 </ul>
