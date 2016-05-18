e478eaa7da76154df4a5c18e34ad263d538487ed
pepesan <pepesan@gmail.com>
2016-05-18 22:51:14 +0200

    incluyendo en el controlador principal una directiva creada en el detalle

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -3,15 +3,16 @@ import { Hero } from './hero';
 @Component({
     selector: 'my-hero-detail',
     template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>`
+  <div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+  </div>
+</div>`
 })
 export class HeroDetailComponent {
+    @Input()
     hero: Hero;
 }
